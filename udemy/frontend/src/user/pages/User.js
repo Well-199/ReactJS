7ee21fac5759386c2c7812a4c5cc0174e0d2ee41
Well-appedidos <wellington.lira@appedidos.com.br>
2022-07-26 14:19:42 -0300

    0.0.1

--- udemy/frontend/src/user/pages/User.js
+++ udemy/frontend/src/user/pages/User.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import UserList from '../components/UsersList'
 
 const Users = () => {
+
+    const USERS = [
+        {
+            id: 'u1', 
+            name: 'Wellington', 
+            image: 'https://github.com/Well-199.png', 
+            places: 3
+        }
+    ]
+
     return(
-        <h2>Users works</h2>
+        <UserList items={USERS}/>
     )
 }
 
