97e8ffca564867d66cec61bad5eb19978a84f767
Well-appedidos <wellington.lira@appedidos.com.br>
2022-11-15 17:09:48 -0300

    0.0.1

---
+++ expense-tracker/src/data/items.ts
@@ -0,0 +1,32 @@
+export const items = [
+    {
+        date: new Date(2022, 11, 10),
+        category: 'food',
+        title: 'McDonalds',
+        value: 32.12
+    },
+    {
+        date: new Date(2022, 10, 18),
+        category: 'food',
+        title: 'Buger King',
+        value: 28
+    },
+    {
+        date: new Date(2022, 10, 20),
+        category: 'rent',
+        title: 'Aluguel Apt',
+        value: 2300
+    },
+    {
+        date: new Date(2022, 11, 3),
+        category: 'salary',
+        title: 'Salário ACME',
+        value: 4500
+    },
+    {
+        date: new Date(2022, 10, 1),
+        category: 'Freelancer',
+        title: 'Página Web with ReactJS',
+        value: 1900
+    }
+]
