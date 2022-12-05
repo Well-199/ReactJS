1b12f719da1e1087869a5522d960f49391693acc
Well-appedidos <wellington.lira@appedidos.com.br>
2022-12-05 09:18:02 -0300

    0.0.1

--- expense-tracker/src/Chat.js
+++ expense-tracker/src/Chat.js
@@ -7,7 +7,7 @@ const Chat = () => {
     // TESTES http://54.159.228.240:4000/
     // LOCAL http://192.168.3.3:4000/
 
-    const socket = io('http://54.159.228.240:4000/', { query: { match: 67 }})
+    const socket = io('http://52.67.82.164/', { query: { match:  67 }})
 
     const [clienteId, setClienteId] = useState(56488)
     const [restauranteId, setRestauranteID] = useState(67)
@@ -18,68 +18,21 @@ const Chat = () => {
 
     useEffect(() => {
 
-        socket.on('attOrders', (data) => {
+        socket.on('receivedMessage', (data) => {
             //setMessage(data.message)
             console.log('SHOW MESSAGE', data)
         })
 
     }, [message])
 
-    async function sendChatMessage() {
-
-        const req = await fetch('http://192.168.3.3:3001/api/salvar-mensagem', {
-            method: 'POST',
-            body: JSON.stringify({
-                cliente_id: clienteId,
-                restaurante_id: restauranteId,
-                chave_pedido: chavePedido,
-                mensagemCliente: message
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': '165765787964209588022835036543'
-            }
-        })
-        const res = await req.json()
-    
-        getMessages()
-    
-      }
-
       function handleMessage(){
 
         if(message !== ''){
-            socket.emit('sendAtt', {message: true})
+            socket.emit('sendMessage', {message: true})
             setMessage('')
         }
 
     }
-    
-    
-      async function getMessages () {
-    
-        const req = await fetch('http://192.168.3.3:3001/api/retorna-mensagens', {
-            method: 'POST',
-            body: JSON.stringify({
-                cliente_id: clienteId,
-                restaurante_id: restauranteId,
-                chave_pedido: chavePedido
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': '165765787964209588022835036543'
-            }
-        })
-        const res = await req.json()
-        setChat(res.result)
-    
-    }
-
-    console.log(chat)
-
-    useEffect(() => {
-        getMessages()
-    }, [])
 
     return(
         <div style={{
@@ -90,23 +43,6 @@ const Chat = () => {
             'alignItems': 'center'
         }}>
             
-
-            {chat.map(item => 
-                <ul style={{ 'listStyle': 'none' }}>
-
-                    {item.comentario &&
-                        <li style={{ 'color' : '#34495e', 'marginBottom': '20px' }}>
-                            Cliente: {item.comentario}
-                        </li>
-                    }
-
-                    {item.observacao && 
-                    <li style={{ 'color' : '#e74c3c' }}>
-                        Restaurante: {item.observacao}
-                    </li>
-                    }
-                </ul>
-            )}
                    
             
 
